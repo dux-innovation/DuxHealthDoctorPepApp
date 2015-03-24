989c08b1c12fc28b48d36418541e728ada221794
Fabio Pagoti <fabiopagoti@gmail.com>
2015-03-24 16:48:53 -0300

    CID10 combo added and dialog box open on save

--- view/main.view.js
+++ view/main.view.js
@@ -197,9 +197,17 @@ sap.ui
 																{
 																	label : "CID-10",
 																	fields : [ new sap.ui.commons.ComboBox(
+																			"combo",
 																			{
-																			// items
-																			// :
+																				// value:
+																				// "{/cid10/0/capitulo/nome}",
+																				items : {
+																					path : "/",
+																					template : new sap.ui.core.ListItem(
+																							{
+																								text : "{}"
+																							})
+																				}
 																			}) ]
 																}) ]
 													}),
@@ -220,8 +228,7 @@ sap.ui
 														title : "Prescrição",
 														formElements : [ new sap.ui.layout.form.FormElement(
 																{
-																	label : "Phone Number",
-																	fields : [ new sap.ui.commons.TextField() ]
+																	fields : [ new sap.ui.commons.TextArea() ]
 																}) ]
 													}),
 
@@ -231,8 +238,7 @@ sap.ui
 														title : "Resultado",
 														formElements : [ new sap.ui.layout.form.FormElement(
 																{
-																	label : "Phone Number",
-																	fields : [ new sap.ui.commons.TextField() ]
+																	fields : [ new sap.ui.commons.TextArea() ]
 																}) ]
 													}),
 											new sap.ui.layout.form.FormContainer(
@@ -244,7 +250,51 @@ sap.ui
 																	fields : [ new sap.ui.commons.Button(
 																			{
 																				text : "Salvar",
-																				style : sap.ui.commons.ButtonStyle.Accept
+																				style : sap.ui.commons.ButtonStyle.Accept,
+																				press : function(
+																						e) {
+																					console
+																							.log("saved");
+																					var dia = new sap.ui.commons.Dialog(
+																							{
+																								width : undefined, // sap.ui.core.CSSSize
+																								height : undefined, // sap.ui.core.CSSSize
+																								scrollLeft : 0, // int
+																								scrollTop : 0, // int
+																								title : "PEP gerado", // string
+
+																								showCloseButton : true, // boolean
+																								resizable : true, // boolean
+																								minWidth : undefined, // sap.ui.core.CSSSize
+																								minHeight : undefined, // sap.ui.core.CSSSize
+																								maxWidth : undefined, // sap.ui.core.CSSSize
+																								maxHeight : undefined, // sap.ui.core.CSSSize
+																								contentBorderDesign : sap.ui.commons.enums.BorderDesign.None, // sap.ui.commons.enums.BorderDesign
+																								modal : false, // boolean
+																								accessibleRole : sap.ui.core.AccessibleRole.Dialog, // sap.ui.core.AccessibleRole
+																								keepInWindow : false, // boolean,
+																														// since
+																														// 1.9.0
+																								autoClose : true, // boolean,
+																													// since
+																													// 1.10
+																								tooltip : undefined, // sap.ui.core.TooltipBase
+																								buttons : [], // sap.ui.core.Control
+																								content : [], // sap.ui.core.Control
+																								defaultButton : undefined, // sap.ui.commons.Button
+																								initialFocus : undefined, // sap.ui.core.Control
+																								closed : [
+																										function(
+																												oEvent) {
+																											var control = oEvent
+																													.getSource();
+																										},
+																										this ]
+																							});
+
+																					dia
+																							.open();
+																				}
 																			}) ]
 																}) ]
 													}),
